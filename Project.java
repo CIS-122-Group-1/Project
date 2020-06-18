dcc5e462ed3b4fff0c1fd04629ff7a405724a0fe
chambies2015 <65683899+chambies2015@users.noreply.github.com>
2020-06-17 20:12:19 -0500

    Add files via upload
    fixed sachi's stolen code for my own.

--- Project.java
+++ Project.java
@@ -1,7 +1,6 @@
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidParameterException;
 import java.util.*;
-import java.text.DecimalFormat;
 
 
 public class Project {
@@ -62,7 +61,7 @@ public class Project {
                 }
                 catch (IllegalArgumentException e)
                 {
-                    System.out.println("Invalid menu choice, try again.");
+                    System.out.println("Invalid menu choice, try again.");                      // catch block for the above error so the program can continue running after letting the user know their error
                 }
 
             }
@@ -76,112 +75,65 @@ public class Project {
         static Map<String, String> StoredSemesters = new HashMap<String, String>();  // initializing a package variable to store semesters
         public static ArrayList<String> temparray = new ArrayList<>(); // initializing a public arraylist so we can recursive print a map.
 
-        public static void GPACalculator() throws InterruptedException          // method that calculates the gpa based off of some variables the user inputs. built upon the original code provided by Sachini
+        public static void GPACalculator() throws InterruptedException          // method that calculates the gpa based off of some variables the user inputs.
         {
-            String lettergrd;
-            double credit;
-            double caltimes = 0;
-            double totalcal = 0;
-            double totalcredit = 0;
-            double finalgpa;
-            final double A = 4.0;
-            final double AMINUS = 3.67;
-            final double BPLUS = 3.33;
-            final double B = 3.0;
-            final double BMINUS = 2.67;
-            final double CPLUS = 2.33;
-            final double C = 2.0;
-            final double CMINUS = 1.67;
-            final double D = 1.00;
-            final double F = 0.0;
 
-            DecimalFormat df = new DecimalFormat("0.##");
-            Scanner grd = new Scanner(System.in);
             Scanner scanner = new Scanner(System.in);
-
-
             System.out.print("What semester do you want to calculate? (2020 fall, 2019 spring, etc): ");
             String semester = scanner.nextLine();                                                       // storing semester that the user wants to input grades for. used to reference stored semesters
 
-            System.out.print("Enter the number of classes taken: ");
+            System.out.print("Enter the number of classes taken: ");                        // prompting user for how many classes they wish to enter grades for, for desired semester
+            int numClasses = scanner.nextInt();
 
-
-            int numClasses = scanner.nextInt();                                                         // prompting user for how many classes they wish to enter grades for, for desired semester
-
-
-
+            float calculatedHourCredit = 0;
+            float overallCreditEarned = 0;
+            float calculatedGPA;
+            double hourCredits = 0;
             for (int i = 0; i < numClasses; i++) {                                                      // for loop to continue the following code for each class the user has taken
 
                 try
                 {
                     System.out.print("Please enter the letter grade for class " + (i+1) + ": ");
-                    lettergrd = grd.next();                                                             // storing letter grade from user input for class num i+1
+                    String inputGrade = scanner.next();                                                             // storing letter grade from user input for class num i+1
 
                     System.out.print("Please enter the number of credits the course gives (1-4): ");
-                    credit = grd.nextDouble();                                                          // storing credits that can be earned from that class
-                    if (credit > 4 || credit < 1)                                                       // checking user input if the inputted credits is valid based off of standard credits
+                    double inputCredit = scanner.nextDouble();                                                          // storing credits that can be earned from that class
+                    overallCreditEarned += inputCredit;                                                 // calculating how many credits the user took in given semester
+                    if (inputCredit > 4 || inputCredit < 1)                                                       // checking user input if the inputted credits is valid based off of standard credits
                     {
                         throw new InvalidAlgorithmParameterException();                                 // if not, throw an error that will terminate the program and state why
                     }
 
+                    if (inputGrade.equalsIgnoreCase("a"))                                       // if block to check the user's input letter grade and to add the appropriate credit/hours for the letter grade.
+                        hourCredits = inputCredit * 4.00;
+                    else if (inputGrade.equalsIgnoreCase("a-"))
+                        hourCredits = inputCredit * 3.67;
+                    else if (inputGrade.equalsIgnoreCase("b+"))
+                        hourCredits = inputCredit * 3.33;
+                    else if (inputGrade.equalsIgnoreCase("b"))
+                        hourCredits = inputCredit * 3.00;
+                    else if (inputGrade.equalsIgnoreCase("b-"))
+                        hourCredits = inputCredit * 2.67;
+                    else if (inputGrade.equalsIgnoreCase("c+"))
+                        hourCredits = inputCredit * 2.33;
+                    else if (inputGrade.equalsIgnoreCase("c"))
+                        hourCredits = inputCredit * 2.00;
+                    else if (inputGrade.equalsIgnoreCase("c-"))
+                        hourCredits = inputCredit * 1.67;
+                    else if (inputGrade.equalsIgnoreCase("d+"))
+                        hourCredits = inputCredit * 1.30;
+                    else if (inputGrade.equalsIgnoreCase("d"))
+                        hourCredits = inputCredit * 1.00;
+                    else if (inputGrade.equalsIgnoreCase("f"))
+                        hourCredits = inputCredit * 0.00;
+                    else
+                    {
+                        throw new InvalidParameterException();                  // if user inputted a letter of any other kind, throw an error
 
-                    switch (lettergrd) {                                                                // switch to determine what gpa the user got for the grade and credits possible
-                        case "A":
-                            caltimes = credit * A;
-                        case "a":
-                            caltimes = credit * A;
-                            break;
-                        case "A-":
-                            caltimes = credit * AMINUS;
-                        case "a-":
-                            caltimes = credit * AMINUS;
-                            break;
-                        case "B+":
-                            caltimes = credit * BPLUS;
-                        case "b+":
-                            caltimes = credit * BPLUS;
-                            break;
-                        case "B":
-                            caltimes = credit * B;
-                        case "b":
-                            caltimes = credit * B;
-                            break;
-                        case "B-":
-                            caltimes = credit * BMINUS;
-                        case "b-":
-                            caltimes = credit * BMINUS;
-                            break;
-                        case "C+":
-                            caltimes = credit * CPLUS;
-                        case "c+":
-                            caltimes = credit * CPLUS;
-                            break;
-                        case "C":
-                            caltimes = credit * C;
-                        case "c":
-                            caltimes = credit * C;
-                            break;
-                        case "C-":
-                            caltimes = credit * CMINUS;
-                        case "c-":
-                            caltimes = credit * CMINUS;
-                            break;
-                        case "D":
-                            caltimes = credit * D;
-                        case "d":
-                            caltimes = credit * D;
-                            break;
-                        case "F":
-                            caltimes = credit * F;
-                        case "f":
-                            caltimes = credit * F;
-                            break;
-                        default:
-                            throw new InvalidParameterException();                                      // if user inputted a letter of any other kind, throw an error
+
                     }
 
-                    totalcredit += credit;                                                 // calculating how many credits the user took in given semester
-                    totalcal += caltimes;                                         // adding all gpa's inputted for the given semester
+                    calculatedHourCredit += hourCredits;                                         // adding all credit/hours earned that were inputted for the given semester
                 }
 
                 catch (InvalidParameterException e)                             // catch error for invalid letter grade that will print the reason why the program terminated
@@ -202,15 +154,15 @@ public class Project {
 
             }
 
-            finalgpa = totalcal / totalcredit;                                      // calculates overall gpa for the given semester
-            System.out.println("GPA for semester "+ semester + ": " + df.format(finalgpa));     //shows the user the overall gpa they achieved for the given semester
+            calculatedGPA = calculatedHourCredit / overallCreditEarned;                                      // calculates overall gpa for the given semester
+            System.out.println("GPA for semester "+ semester + ": " + calculatedGPA);     //shows the user the overall gpa they achieved for the given semester
             System.out.println("\n");
-            String semesterGPA = semester + ": " + df.format(finalgpa);         // stores users gpa/semester string into a variable
+            String semesterGPA = semester + ": " + calculatedGPA;         // stores users gpa/semester string into a variable
             Thread.sleep(1000);
 
             System.out.print("Would you like to store this semesters GPA?(y or n): ");                          // asking the user if they want to store the semester for reference later
             String choice = scanner.next();
-            String gpa = df.format(finalgpa);
+            String gpa = String.valueOf(calculatedGPA);
             switch (choice) {                                                                                   // switch for the choice the user makes
                 case "y" -> addSemester(semester, gpa);                                                         // if y then we call the addsemester method to store it into the package variable StoredSemesters
                 case "Y" -> addSemester(semester, gpa);
@@ -304,7 +256,7 @@ public class Project {
 
 
 
-//        public static void printSemesters()                                 // method to print all semesters that have been stored
+        //        public static void printSemesters()                                 // method to print all semesters that have been stored
 //        {
 //            if (StoredSemesters.size() > 0)                                 // checking if any semesters have been stored
 //            {
@@ -402,10 +354,10 @@ public class Project {
                     double temp = Double.parseDouble(stringtemp);
                     sumOfCredits += temp;
                 }
-                double finalgpa = sumOfCredits / numGPAS;                           // expression that calculates the overall gpa from all semesters / number of semesters
+                double calculatedGPA = sumOfCredits / numGPAS;                           // expression that calculates the overall gpa from all semesters / number of semesters
 
 
-                System.out.println("Total GPA from all semesters stored: " + finalgpa);
+                System.out.println("Total GPA from all semesters stored: " + calculatedGPA);
                 System.out.println();
             }
             else
